85994a2cc19efba2a0bbe41034d4889cd5ab2ce8
nouman-terafort <nouman0027oo@gmail.com>
2025-03-19 14:33:18 +0500

    setting of dto and responce modal

--- app/src/main/java/com/example/eshaafi_healthunit/app/utils/Constant.kt
+++
@@ -1,9 +0,0 @@
-package com.example.eshaafi_healthunit.app.utils
-
-class Constant {
-    companion object {
-//        my api end points
-        const val BASE_URL: String = "https://api.eshaafi.com/"
-        const val CITIES_API: String = "new/cities"
-    }
-}
--- eshaafiHU_Android_SDK/src/main/java/com/example/eshaafihu_android_sdk/feature/cities/data/usecase/citiesUseCaseImpl.kt
+++ eshaafiHU_Android_SDK/src/main/java/com/example/eshaafihu_android_sdk/feature/cities/data/usecase/citiesUseCaseImpl.kt
@@ -1,16 +1,15 @@
-package com.example.eshaafihu_android_sdk.feature.cities.domain.usecase
+package com.example.eshaafihu_android_sdk.feature.cities.data.usecase
 
 import com.example.eshaafihu_android_sdk.core.network.dataState.DataState
-import com.example.eshaafihu_android_sdk.feature.cities.data.model.CitiesResponseModelDto
-import com.example.eshaafihu_android_sdk.core.usecase.CitiesUseCase
+import com.example.eshaafihu_android_sdk.feature.cities.domain.entity.CitiesEntityResponseModel
+import com.example.eshaafihu_android_sdk.feature.cities.domain.usecase.CitiesUseCase
 import com.example.eshaafihu_android_sdk.feature.cities.domain.repository.CitiesRepository
 import javax.inject.Inject
 
 class CitiesUseCaseImpl @Inject constructor(
     private val repository: CitiesRepository
 ) : CitiesUseCase {
-
-    override suspend fun getCities(): DataState<CitiesResponseModelDto> {
+    override suspend fun getCities(): DataState<CitiesEntityResponseModel> {
         return repository.getCities()
     }
 }
