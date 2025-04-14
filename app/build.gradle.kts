3c5e4273740cdf5b04cbf3262428ded16c87476d
Nouman <130449205+nouman-terafort@users.noreply.github.com>
2025-04-14 09:52:47 +0500

    Dev nouman (#9)
    * Update jitpack.yml
    
    * setting of dto and responce modal
    
    * merge (#6)
    
    * Update README.md
    
    * Dev ali (#5)
    
    * Update jitpack.yml
    
    * merge (#4)
    
    * Update jitpack.yml (#2)
    
    * Dev nouman (#3)
    
    * Update jitpack.yml
    
    * setting of dto and responce modal
    
    ---------
    
    Co-authored-by: aliwaseem72 <ali.waseem@itinsync.com>
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Nouman <130449205+nouman-terafort@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nouman <130449205+nouman-terafort@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nouman <130449205+nouman-terafort@users.noreply.github.com>
    
    * hiding irrelevant data from user continue...
    
    * hiding data form user
    
    * setup new api with post method
    
    * adding M3 Lib
    
    ---------
    
    Co-authored-by: aliwaseem72 <ali.waseem@itinsync.com>
    Co-authored-by: Ali Waseem <37467686+aliwaseem72@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.graphics)
 
     // Jetpack Compose dependencies
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.material)
+//    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.tooling.preview)
     implementation(libs.androidx.compose.viewmodel)
     implementation(libs.androidx.compose.activity)
